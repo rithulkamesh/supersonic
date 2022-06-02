40d667dc5e8797ad261335b39ee19f8106c5e7df
Rithul Kamesh <hi@rithul.dev>
2022-06-03 00:07:35 +0530

    Database + Basic Mailer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate dotenv_codegen;
+
+use dotenv::dotenv;
+extern crate dotenv;
+use lettre::{ SmtpTransport };
+use sqlx::mysql::MySqlPoolOptions;
+
+#[async_std::main]
+async fn main() -> Result<(), sqlx::Error> {
+    dotenv().ok();
+
+    let _mailer = SmtpTransport::unencrypted_localhost();
+    let pool = MySqlPoolOptions::new().connect(dotenv!("DATABASE_URL")).await?;
+
+    let q= sqlx::query("show tables;").execute(&pool).await?;
+    eprintln!("{:?}", q);
+
+    Ok(())
 }
